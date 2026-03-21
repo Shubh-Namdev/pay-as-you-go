bad21661ebfb8fe92aca17da48ae23d0d46c0591
Shubh-Namdev <s.namdev013@gmail.com>
2026-03-21 11:46:33 +0530

    dtos added to protect entity from external

---
+++ src/main/java/com/sunking/payg/dto/CustomerResponse.java
@@ -0,0 +1,12 @@
+package com.sunking.payg.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CustomerResponse {
+    private Long id;
+    private String name;
+    private String phone;
+}
