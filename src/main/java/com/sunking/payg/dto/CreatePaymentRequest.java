2160dd3e4ad5a393e67cf2c77a87192a599fdc96
Shubh-Namdev <s.namdev013@gmail.com>
2026-03-24 10:00:00 +0530

    added property to achiveve idempotency

--- src/main/java/com/sunking/payg/dto/CreatePaymentRequest.java
+++ src/main/java/com/sunking/payg/dto/CreatePaymentRequest.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 
 @Data
 public class CreatePaymentRequest {
+    private String idempotencyKey;
     private Long customerId;
     private Long deviceId;
     private BigDecimal amount;
