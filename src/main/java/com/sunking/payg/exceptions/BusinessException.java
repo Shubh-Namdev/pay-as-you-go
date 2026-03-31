4f91deba0b82aad4f484380b0f27cf036578806a
shubham namdev <s.namdev013@gmail.com>
2026-03-31 10:00:00 +0530

    global exception handler implemented for handling exceptions globally

---
+++ src/main/java/com/sunking/payg/exceptions/BusinessException.java
@@ -0,0 +1,8 @@
+package com.sunking.payg.exceptions;
+
+public class BusinessException extends RuntimeException{
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/sunking/payg/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.sunking.payg.exceptions;
+
+
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
