2bdc56cbb3548a7847068662981b2669cce392fc
shubham namdev <s.namdev013@gmail.com>
2026-04-01 10:00:00 +0530

    security configuration has been added

---
+++ src/main/java/com/sunking/payg/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.sunking.payg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.sunking.payg.jwt.JwtFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                // disable CSRF (because REST + JWT)
+                .csrf(csrf -> csrf.disable())
+
+                // no sessions (stateless system)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                // define API access rules
+                .authorizeHttpRequests(auth -> auth
+
+                        // public APIs
+                        .requestMatchers("/auth/login", "/users", "/devices").permitAll()
+
+                        // everything else
+                        .anyRequest().authenticated()
+                )
+
+                // add JWT filter
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
