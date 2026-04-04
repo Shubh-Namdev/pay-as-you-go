edb9c7bd26a33bacdc048fafab72a2d0ccf6707b
shubham namdev <s.namdev013@gmail.com>
2026-04-04 12:09:55 +0530

    authentication implemented using jwt

---
+++ src/main/java/com/sunking/payg/jwt/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.sunking.payg.jwt;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "bXktc3VwZXItc2VjdXJlLWp3dC1rZXktdGhhdC1pcy1sb25nLWVub3VnaC0xMjM0NTY3ODkw";
+
+    public String generateToken(String userId, String role) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 day
+                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .compact();
+    }
+
+    public String extractPhone(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
