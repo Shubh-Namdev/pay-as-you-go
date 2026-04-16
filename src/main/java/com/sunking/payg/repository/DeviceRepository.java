1ad55b281c40ec9d4e9ec802d6486c1ed5c40614
shubham namdev <s.namdev013@gmail.com>
2026-04-16 10:00:00 +0530

    code has been updated as new api added to support device management

--- src/main/java/com/sunking/payg/repository/DeviceRepository.java
+++ src/main/java/com/sunking/payg/repository/DeviceRepository.java
@@ -1,8 +1,18 @@
 package com.sunking.payg.repository;
 
 import com.sunking.payg.entity.Device;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
 
 public interface DeviceRepository extends JpaRepository<Device, Long> {
     
+    Optional<Device> findById(Long id);
+    Page<Device> findByAvailable(boolean available, Pageable pageable);
 }
